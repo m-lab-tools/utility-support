c731c5bea9372d64bf0a4937ae56a0ae69b00070
stephen soltesz <stephen.soltesz@gmail.com>
2013-07-13 21:10:05 +0000

    provide full path to import config.sh

--- init/start.sh
+++ init/start.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:$SLICEHOME/bin:$SLICEHOME/sbin
 export LD_LIBRARY_PATH=$SLICEHOME/lib:$LD_LIBRARY_PATH
 
 # NOTE: start the service if it is not already running.
-source config.sh
+source $SLICEHOME/init/config.sh
 echo "Starting servers:"
 for port in $UDP_PORT_LIST ; do
     start_ncat $port --udp
--- init/stop.sh
+++ init/stop.sh
@@ -4,7 +4,7 @@ source /etc/mlab/slice-functions
 set -e
 
 # NOTE: kill the service if it is running.
-source config.sh
+source $SLICEHOME/init/config.sh
 echo "Stopping servers:"
 for port in $UDP_PORT_LIST ; do
     stop_ncat $port --udp
